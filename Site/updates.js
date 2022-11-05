509d0e4fb873bd4f778297303e407ebd774f6428
Zorhi <72230177+Zorhi@users.noreply.github.com>
2022-11-05 13:17:20 -0500

    Update updates.js

--- Site/updates.js
+++ Site/updates.js
@@ -1,5 +1,5 @@
 var updates = [
-    {message: "Welcome to the 3rd official release of Andyum!"}
+    {message: "Welcome to the 1st Official Rodriguez Middle School Proxy"}
 ];
 var updatespage = document.getElementById("updatespage");
 for(let item of updates) {
